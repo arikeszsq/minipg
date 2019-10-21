d081f9eb0711722cdb032b21512fa154bf4a0e27
zhusq <857355787@qq.com>
2019-10-21 11:33:41 +0800

    12

--- modules/api/controllers/ActivityController.php
+++ modules/api/controllers/ActivityController.php
@@ -5,11 +5,17 @@ namespace app\modules\api\controllers;
 
 
 use app\models\Activity;
+use app\models\UserActivityCollect;
+use app\models\UserActivityEnroll;
 use app\models\UserInfo;
 use Yii;
 
 class ActivityController extends BaseController
 {
+    /**
+     * 活动列表
+     * @return array|\yii\db\ActiveRecord|null
+     */
     public function actionLists()
     {
         $user = $this->requireLoginUser();
@@ -39,7 +45,64 @@ class ActivityController extends BaseController
                 'detail_list' => $activities,
             ],
         ];
+    }
 
+    /**
+     * 收藏活动
+     * @return array|\yii\db\ActiveRecord|null
+     */
+    public function actionCollect()
+    {
+        $user = $this->requireLoginUser();
+        if ($user['code'] != 200) {
+            return $user;
+        } else {
+            $user_id = $user['user_id'];
+        }
+        $activity_id = Yii::$app->request->post('activity_id');
+        $user_activity_collect = new UserActivityCollect();
+        $user_activity_collect->user_id = $user_id;
+        $user_activity_collect->activity_id = $activity_id;
+        if ($user_activity_collect->save()) {
+            return [
+                'code' => 200,
+                'msg' => '收藏成功'
+            ];
+        } else {
+            return [
+                'code' => 100,
+                'msg' => '收藏失败'
+            ];
+        }
+    }
+
+    /**
+     * 活动报名
+     * @return array|\yii\db\ActiveRecord|null
+     */
+    public function actionEnroll()
+    {
+        $user = $this->requireLoginUser();
+        if ($user['code'] != 200) {
+            return $user;
+        } else {
+            $user_id = $user['user_id'];
+        }
+        $activity_id = Yii::$app->request->post('activity_id');
+        $user_activity_enroll = new UserActivityEnroll();
+        $user_activity_enroll->user_id = $user_id;
+        $user_activity_enroll->activity_id = $activity_id;
+        if ($user_activity_enroll->save()) {
+            return [
+                'code' => 200,
+                'msg' => '收藏成功'
+            ];
+        } else {
+            return [
+                'code' => 100,
+                'msg' => '收藏失败'
+            ];
+        }
     }
 
 }
