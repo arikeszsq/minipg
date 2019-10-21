f72360192e90be0126c88be34d2707bf70a0bc45
zhusq <857355787@qq.com>
2019-10-21 10:51:10 +0800

    8

---
+++ modules/api/controllers/ActivityController.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace app\modules\api\controllers;
+
+
+use app\models\Activity;
+use Yii;
+
+class ActivityController extends BaseController
+{
+    public function actionLists()
+    {
+        $inputs = Yii::$app->request->get();
+        $page = $inputs['page'] ?? 1;
+        $per_page = $inputs['per_page'] ?? 10;
+        $query = Activity::find();
+        $total_count = $query->count();
+        $total_page = ceil($total_count / $per_page);
+        $offset = ($page - 1) * $per_page;
+        $activities = $query
+            ->offset($offset)
+            ->limit($per_page)
+            ->all();
+        return [
+            'code' => 200,
+            'msg' => '获取成功',
+            'data' => [
+                'total_count' => $total_count,
+                'total_page' => $total_page,
+                'detail_list' => $activities,
+            ],
+        ];
+
+    }
+
+}
--- modules/api/controllers/BaseController.php
+++ modules/api/controllers/BaseController.php
@@ -5,10 +5,31 @@ namespace app\modules\api\controllers;
 
 
 use Yii;
-use yii\filters\Cors;
 use yii\web\Controller;
 use yii\web\Response;
 
 class BaseController extends Controller
 {
+    public $layout = false;
+    public $enableCsrfValidation = false;
+    public $formSubmit = false;
+    protected $jsoncallback = '';
+
+    public function afterAction($action, $result)
+    {
+        if ($this->formSubmit) {
+            Yii::$app->response->format = Response::FORMAT_HTML;
+            $result = "<script></script>";
+        }
+        $this->jsoncallback = Yii::$app->request->get('jsoncallback', '');
+        if ($this->jsoncallback) {
+            Yii::$app->response->format = Response::FORMAT_JSONP;
+            $result = ['callback' => $this->jsoncallback, 'data' => $result];
+        } else {
+            if (Yii::$app->response->format != Response::FORMAT_RAW) {
+                Yii::$app->response->format = Response::FORMAT_JSON;
+            }
+        }
+        return parent::afterAction($action, $result);
+    }
 }
