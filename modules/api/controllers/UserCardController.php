556f866796d373666d91d9e2da966aded1d7f3d5
zhusq <857355787@qq.com>
2019-10-24 17:41:43 +0800

    我的会员卡

--- modules/api/controllers/UserCardController.php
+++ modules/api/controllers/UserCardController.php
@@ -7,6 +7,7 @@ namespace app\modules\api\controllers;
 use app\models\Card;
 use app\models\UserCard;
 use Yii;
+use yii\helpers\ArrayHelper;
 
 class UserCardController extends BaseController
 {
@@ -24,20 +25,22 @@ class UserCardController extends BaseController
         $total_count = $query->count();
         $total_page = ceil($total_count / $per_page);
         $offset = ($page - 1) * $per_page;
-
         $user_cards = $query
             ->where(['user_id' => $user_id])
             ->offset($offset)
             ->limit($per_page)
-            ->with('card')
+            ->asArray()
             ->all();
+        $card_ids = arrayHelper::getColumn($user_cards, 'card_id');
+        $other_cards = Card::find()->where(['not in', 'id', $card_ids])->asArray()->all();
         return [
             'code' => 200,
             'msg' => '获取成功',
             'data' => [
                 'total_count' => $total_count,
                 'total_page' => $total_page,
-                'detail_list' => $user_cards
+                'detail_list' => $user_cards,
+                'other_cards' => $other_cards
             ],
         ];
     }
