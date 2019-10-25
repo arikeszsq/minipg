a55c475dee79536998bb5a6da4110ae1a242d8c1
zhusq <857355787@qq.com>
2019-10-25 18:35:13 +0800

    用户信息

--- modules/api/controllers/UserInfoController.php
+++ modules/api/controllers/UserInfoController.php
@@ -5,27 +5,62 @@ namespace app\modules\api\controllers;
 
 
 use app\models\UserCard;
+use app\models\UserInfo;
 use Yii;
 
 class UserInfoController extends BaseController
 {
-    public function actionAdd()
+    /**
+     * 修改用户资料
+     * @return array
+     */
+    public function actionUpdate()
     {
-        $user_info = new UserCard();
-        $user_info->load(Yii::$app->request->post());
-        if ($user_info->save()) {
+        $ret = $this->requireLogin();
+        if ($ret['code'] != 200) {
+            return $ret;
+        }
+        $open_id = $ret['open_id'];
+        $user = $this->getUser($open_id);
+        $user->load(Yii::$app->request->post());
+        if ($user->save()) {
             return [
                 'code' => 200,
-                'msg' => '保存成功',
-                'data' => $user_info
+                'msg' => '成功',
+                'data' => $user
             ];
         } else {
             return [
                 'code' => 101,
-                'msg' => '保存失败'
+                'msg' => '失败'
             ];
         }
     }
 
+    /**
+     * 用户详情
+     * @return array
+     */
+    public function actionDetail()
+    {
+        $ret = $this->requireLogin();
+        if ($ret['code'] != 200) {
+            return $ret;
+        }
+        $open_id = $ret['open_id'];
+        $user = $this->getUser($open_id);
+        if ($user) {
+            return [
+                'code' => 200,
+                'msg' => '成功',
+                'data' => $user
+            ];
+        } else {
+            return [
+                'code' => 101,
+                'msg' => '失败'
+            ];
+        }
+    }
 
 }
