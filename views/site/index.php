80e0df38f2430f28877f219eeb7a9b32964725f1
zhusq <857355787@qq.com>
2019-10-28 14:09:34 +0800

    66

--- views/site/index.php
+++ views/site/index.php
@@ -4,7 +4,13 @@
 
 $this->title = '小程序管理后台';
 ?>
-<div class="site-index">
+<style>
+    * { margin: 0; padding: 0; }
+    html { height: 100%; }
+    body { height: 100%; background: #fff url(images/backgroud.png) 50% 50% no-repeat; background-size: cover;}
+</style>
+
+<div class="site-index" style="background-image: url("")">
     <div class="jumbotron">
         <h1>欢迎来到管理后台!</h1>
         <p><a class="btn btn-lg btn-success" href="/admin/event/index">进入</a></p>
