bc555fb1c2403847b36c268c3791d14847719123
zhusq <857355787@qq.com>
2019-10-31 16:26:52 +0800

    2

---
+++ modules/api/controllers/CouponController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace app\modules\api\controllers;
+
+
+use app\models\Card;
+use app\models\Coupon;
+use app\models\UserCard;
+use Yii;
+
+class CouponController extends BaseController
+{
+    /**
+     * 优惠券详情
+     * @return array
+     */
+    public function actionDetail()
+    {
+        $inputs = Yii::$app->request->post();
+        $coupon_id = $inputs['coupon_id'];
+        $coupon = Coupon::find()
+            ->where(['id' => $coupon_id])
+            ->asArray()
+            ->one();
+        return [
+            'code' => 200,
+            'msg' => '成功获取',
+            'coupon' => $coupon,
+        ];
+    }
+}
