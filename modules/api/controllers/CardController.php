ced1e2c54150efbe09cb278723d82e31a0c20e4f
zhusq <857355787@qq.com>
2019-10-31 11:27:49 +0800

    判断当前卡是否是会员

--- modules/api/controllers/CardController.php
+++ modules/api/controllers/CardController.php
@@ -5,6 +5,7 @@ namespace app\modules\api\controllers;
 
 
 use app\models\Card;
+use app\models\UserCard;
 use Yii;
 
 class CardController extends BaseController
@@ -39,8 +40,21 @@ class CardController extends BaseController
      */
     public function actionDetail()
     {
+        $ret = $this->requireLogin();
+        if ($ret['code'] != 200) {
+            return $ret;
+        }
+        $open_id = $ret['open_id'];
+        $user = $this->getUser($open_id);
+        $user_id = $user->id;
         $inputs = Yii::$app->request->post();
         $card_id = $inputs['card_id'];
+        $user_card = UserCard::find()->where(['user_id' => $user_id])->andWhere(['card_id' => $card_id])->one();
+        if ($user_card) {
+            $is_vip = 1;
+        } else {
+            $is_vip = 0;
+        }
         $card = Card::find()
             ->with('coupons')
             ->with('events')
@@ -51,6 +65,7 @@ class CardController extends BaseController
             'code' => 200,
             'msg' => '成功获取',
             'card' => $card,
+            'is_vip' => $is_vip
         ];
     }
 
