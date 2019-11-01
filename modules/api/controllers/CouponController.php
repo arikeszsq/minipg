bc3040ebe0fd6392b5421851576b397755a4ddfa
zhusq <857355787@qq.com>
2019-11-01 13:57:47 +0800

    修复优惠券bug

--- modules/api/controllers/CouponController.php
+++ modules/api/controllers/CouponController.php
@@ -17,6 +17,13 @@ class CouponController extends BaseController
      */
     public function actionDetail()
     {
+        $ret = $this->requireLogin();
+        if ($ret['code'] != 200) {
+            return $ret;
+        }
+        $open_id = $ret['open_id'];
+        $user = $this->getUser($open_id);
+        $user_id = $user->id;
         $inputs = Yii::$app->request->post();
         $coupon_id = $inputs['coupon_id'];
         $coupon = Coupon::find()
@@ -24,10 +31,18 @@ class CouponController extends BaseController
             ->with('business')
             ->asArray()
             ->one();
+        $card_id = $coupon['card_id'];
+        $user_card = UserCard::find()->where(['user_id' => $user_id])->andWhere(['card_id' => $card_id])->one();
+        if ($user_card) {
+            $is_vip = 1;
+        } else {
+            $is_vip = 0;
+        }
         return [
             'code' => 200,
             'msg' => '成功获取',
             'coupon' => $coupon,
+            'is_vip' => $is_vip
         ];
     }
 }
