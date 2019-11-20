04feae4527e26e89d81a2da2fca6c485fb40d53b
zhusq <857355787@qq.com>
2019-11-20 14:21:54 +0800

    1

--- modules/api/controllers/NotifyController.php
+++ modules/api/controllers/NotifyController.php
@@ -13,14 +13,11 @@ class NotifyController extends BaseController
 
     public function actionIndex()
     {
-
         var_dump($_REQUEST);
         $xml = file_get_contents('php://input');
         libxml_disable_entity_loader(true);
         $result = $this->xmlstr_to_array($xml);
         $result_code = $result['result_code'];
     }
-
-    public function
 
 }
